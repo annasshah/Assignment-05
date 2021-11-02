dc44dad02d15cf6bfcf45961593b4a12ff0509a3
annasshah <anasshahskype@gmail.com>
2021-11-02 12:10:40 +0500

    Assignment # 05

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import './App.css';
 import SliderMainSection from './components/SliderMainSection';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import AfterSliderMain from './components/AfterSliderMain';
+import { Box } from '@mui/system';
 
 function App() {
+
   return (
     <>
+    <Box sx={{backgroundColor:'#f2f3f7',height:'100%'}}>
+
+
       <Navbar />
       
       <SliderMainSection/>
 
+      <AfterSliderMain/>
+  
+
+    </Box>
     </>
   );
 }
--- src/components/SliderMainSection.js
+++ src/components/SliderMainSection.js
@@ -11,8 +11,8 @@ export default function SliderMainSection() {
     return (
 
         <>
-            <Container>
-                <Paper elevation={2} sx={{ my: 3, py: 2 }} square={true} children={
+            <Container sx={{ my: 1, py: 2 }} >
+                <Paper elevation={2} square={true} children={
                     <Grid container>
                         <Hidden mdDown={true}>
                             <Grid item md={3} lg={3}>
@@ -30,19 +30,19 @@ export default function SliderMainSection() {
 
                             <Grid container spacing={2} sx={{ my: 1 }}>
 
-                                <Grid item md={3} lg={3} sm={3}>
+                                <Grid item md={6} lg={3} sm={6} xs={12}>
                                     <ProBuyerExective />
                                 </Grid>
 
-                                <Grid item md={3} lg={3} sm={3}>
+                                <Grid item md={6} lg={3} sm={6} xs={12}>
                                     <HomeGarden2021 />
                                 </Grid>
 
-                                <Grid item md={3} lg={3} sm={3}>
+                                <Grid item md={6} lg={3} sm={6} xs={12}>
                                     <PromotionCard title={'Digital Booths'} imgSrc={'https://i.alicdn.com/img/imgextra/i3/O1CN01i1pN3924J3K92CAMq_!!6000000007369-2-tps-350-350.png'} />
                                 </Grid>
 
-                                <Grid item md={3} lg={3} sm={3}>
+                                <Grid item md={6} lg={3} sm={6} xs={12}>
                                     <PromotionCard title={'Top-Ranking Products'} imgSrc={'https://i.alicdn.com/img/imgextra/i2/O1CN01IlZLHL1h7O4Zn6rIX_!!6000000004230-2-tps-350-350.png'} />
                                 </Grid>
                             </Grid>
